ef6cf24a81b55a7cc65ca0f824226cbfc7f1d533
Shruti <60984268+shruti2126@users.noreply.github.com>
2022-03-02 10:29:37 -0600

    Update btree.cpp

--- src/btree.cpp
+++ src/btree.cpp
@@ -19,55 +19,67 @@
 
 //#define DEBUG
 
-namespace badgerdb
-{
+namespace badgerdb {
 
 // -----------------------------------------------------------------------------
 // BTreeIndex::BTreeIndex -- Constructor
 // -----------------------------------------------------------------------------
 
-BTreeIndex::BTreeIndex(const std::string &relationName,
-					   std::string &outIndexName,
-					   BufMgr *bufMgrIn,
-					   const int attrByteOffset,
-					   const Datatype attrType)
+BTreeIndex::BTreeIndex(const std::string & relationName,
+		std::string & outIndexName,
+		BufMgr *bufMgrIn,
+		const int attrByteOffset,
+		const Datatype attrType)
 {
 	std::ostringstream idxStr;
 	idxStr << relationName << '.' << attrByteOffset;
-	outIndexName = idxStr.str();
-
+	outIndexName = idxStr.str(); 
+	
 	// Check to see if the corresponding index file exists. If so, open the file.
-	// If not, create it
-	try
-	{
+	//If not, create it 
+	try {
 		BlobFile::open(relationName);
 	}
 	catch (FileNotFoundException &e)
 	{
-		file = new BlobFile(outIndexName, true);
-		Page *headerPage;
-		bufMgrIn->allocPage(file, headerPageNum, headerPage);
-		// insert metadata in header page
-		IndexMetaInfo *metadata = (IndexMetaInfo *)headerPage;
+		 file = new BlobFile(outIndexName, true);
+		 Page *headerPage;
+		 bufMgrIn->allocPage(file, headerPageNum, headerPage);
+		 //insert metadata in header page
+		 IndexMetaInfo *metadata = (IndexMetaInfo *)headerPage;
 
-		strcpy(metadata->relationName, relationName.c_str());
-		metadata->attrByteOffset = attrByteOffset;
-		metadata->attrType = attrType;
-		metadata->rootPageNo = rootPageNum;
+		 strcpy(metadata->relationName, relationName.c_str());
+		 metadata->attrByteOffset = attrByteOffset;
+		 metadata->attrType = attrType;
+		 metadata->rootPageNo = rootPageNum;
 
-		Page *rootPage;
-		bufMgrIn->allocPage(file, rootPageNum, rootPage);
+		 Page *rootPage;
+		 bufMgrIn->allocPage(file, rootPageNum, rootPage);
+		 
+		 //initialize root
+		 LeafNodeInt *root = (LeafNodeInt *)rootPage;
 
-		// insert entries for every tuple in the base relation using FileScan class.
-		FileScan *scanner = new FileScan(relationName, bufMgrIn);
-		std::string currRecord = scanner->getRecord();
+		 // insert entries for every tuple in the base relation using FileScan class.
+		 FileScan * scanner = new FileScan(relationName, bufMgrIn);
+		 std::string currRecord = scanner->getRecord();
+		 RecordId recordId;
+		 try
+		 {
+			 while (true)
+			 {
+				
+				scanner->scanNext(recordId);
+				const char *key = currRecord.c_str() + attrByteOffset;
 
-		// loop through every record in the file that contains the the relation (relationName)
-		//   while() {
+				//std::string *key = &currRecord + attrByteOffset;
+				insertEntry(key, recordId);
+			 }
+		} catch(EndOfFileException &e) {
 
-		//  }
-	}
-}
+		}
+    }
+}  
+
 
 // -----------------------------------------------------------------------------
 // BTreeIndex::~BTreeIndex -- destructor
@@ -76,22 +88,64 @@ BTreeIndex::BTreeIndex(const std::string &relationName,
 BTreeIndex::~BTreeIndex()
 {
 	scanExecuting = false;
-	bufMgr->flushFile(file);
-	delete file;
+  	bufMgr->flushFile(file);
+  	delete file;
 }
 
 // -----------------------------------------------------------------------------
 // BTreeIndex::insertEntry
 // -----------------------------------------------------------------------------
+  /**
+	 * Insert a new entry using the pair <value,rid>. 
+	 * Start from root to recursively find out the leaf to insert the entry in. The insertion may cause splitting of leaf node.
+	 * This splitting will require addition of new leaf page number entry into the parent non-leaf, which may in-turn get split.
+	 * This may continue all the way upto the root causing the root to get split. If root gets split, metapage needs to be changed accordingly.
+	 * Make sure to unpin pages as soon as you can.
+   * @param key			Key to insert, pointer to integer/double/char string
+   * @param rid			Record ID of a record whose entry is getting inserted into the index.
+	**/
 
-void BTreeIndex::insertEntry(const void *key, const RecordId rid) 
+void BTreeIndex::insertEntry(const void *key, const RecordId rid)
 {
 
+	Page *root;
+	bufMgr->readPage(file, rootPageNum, root);
+
+	//if leaf node, make helper for insert in leaf Node
+	//compare root page no to starting root page number, if true
+
+	RIDKeyPair<int> pair;
+	pair.set(rid, (*((int *)key)));
+
+
 }
+
+
+void BTreeIndex::insertToLeaf(current node, current node's number (page id), key value pair)
+
+
+void BTreeIndex::insertToNonLeaf()
+
+
 
 // -----------------------------------------------------------------------------
 // BTreeIndex::startScan
 // -----------------------------------------------------------------------------
+  /**
+	 * Begin a filtered scan of the index.  For instance, if the method is called 
+	 * using ("a",GT,"d",LTE) then we should seek all entries with a value 
+	 * greater than "a" and less than or equal to "d".
+	 * If another scan is already executing, that needs to be ended here.
+	 * Set up all the variables for scan. Start from root to find out the leaf page that contains the first RecordID
+	 * that satisfies the scan parameters. Keep that page pinned in the buffer pool.
+   * @param lowVal	Low value of range, pointer to integer / double / char string
+   * @param lowOp		Low operator (GT/GTE)
+   * @param highVal	High value of range, pointer to integer / double / char string
+   * @param highOp	High operator (LT/LTE)
+   * @throws  BadOpcodesException If lowOp and highOp do not contain one of their their expected values 
+   * @throws  BadScanrangeException If lowVal > highval
+	 * @throws  NoSuchKeyFoundException If there is no key in the B+ tree that satisfies the scan criteria.
+	**/
 
 void BTreeIndex::startScan(const void* lowValParm,
 				   const Operator lowOpParm,
@@ -116,7 +170,14 @@ void BTreeIndex::scanNext(RecordId& outRid)
 //
 void BTreeIndex::endScan() 
 {
-
+	if (scanExecuting == false) {
+		throw ScanNotInitializedException();
+	} else {
+		scanExecuting == false;
+		bufMgr->unPinPage(file, currentPageNum, false);
+	}
+  
 }
+
 
 }
